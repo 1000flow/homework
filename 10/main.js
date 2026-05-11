71834742d56ed0d59e12b308a58d57a51a560353
1000flow <danilmukash@gmail.com>
2026-05-11 12:46:02 +0300

    Add files via upload

---
+++ 10/main.js
@@ -0,0 +1,17 @@
+const firstName = "Иван ";
+const lastName = "Иванов";
+const isStudent = true;
+const age = 19;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+console.log(
+	`Меня зовут ${firstName + lastName}, мне ${age} лет. Я ученик/ученица курса: ${isStudent}`,
+);
+// Какое значение будет у переменной result?
+// let a = "123";
+// let b = +"456";
+// let c = Number("789");
+// let d = Boolean(0);
+// let e = Boolean(" ");
+// let result = a + b + c + d + e;
+// будет значение: 123456789falsetrue
