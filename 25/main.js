a2c1b5c27cfd61a2808329d29a02aa83c14cdedd
1000flow <danilmukash@gmail.com>
2026-05-18 17:41:28 +0300

    Add files via upload

---
+++ 25/main.js
@@ -0,0 +1,26 @@
+// Задача 1
+// for (i = 1; i <= 20; i++) {
+// 	if (i % 4 == 0) {
+// 		console.log(i);
+// 	}
+// }
+// Задача 2
+// let number = +prompt("Введите число");
+// let result = 1;
+// if (number < 0 || isNaN(number)) {
+// 	alert("Введите коректное число");
+// }
+// for (let i = 1; i <= number; i++) {
+// 	console.log(i);
+// 	result = result * i;
+// }
+// alert(result);
+// Задача 3 не понял как делать
+// const board = 8;
+// for (let i = 0; i < board; i++) {
+// 	let row = "";
+// 	for (let j = 0; j < board; j++) {
+// 		row += "* ";
+// 	}
+// 	console.log(row);
+// }
